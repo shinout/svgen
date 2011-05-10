67e6d198796356f79975bcd33f2dbd05fc639b82
SHIN Suzuki <shinout310@gmail.com>
2011-05-10 15:31:07 +0900

    support rname(tsv2break)

--- tsv2break.js
+++ tsv2break.js
@@ -1,8 +1,10 @@
 const LS = require('linestream');
+const AP = require('argparser');
 
 function main() {
+  var p = new AP().parse();
 
-  var tsvfile = process.argv[2];
+  var tsvfile = p.getArgs(0);
   
   if (! require('path').existsSync(tsvfile)) {
     process.stderr.write(tsvfile + ': No such file.');
@@ -15,11 +17,12 @@ function main() {
   ls.on('data', function(line) {
     if (!line || line.charAt(0) == '#') return;
     tsvdata = line.split('\t');
-    if (tsvdata.length < 3) return;
+    if (tsvdata.length < 4) return;
 
     var type   = tsvdata[0];
     var start  = Number(tsvdata[1]);
     var length = Number(tsvdata[2]);
+    var rname  = tsvdata[3];
 
     if (start <= 0 || length <= 0) return;
 
@@ -36,15 +39,15 @@ function main() {
       bpR = start; 
       break;
     case 'INV':
-      process.stdout.write('R' + '\t' + start + '\t' + type + '\n');
-      process.stdout.write('L' + '\t' + start + '\t' + type + '\n');
+      process.stdout.write('R' + '\t' + start + '\t' + type + '\t' + rname + '\n');
+      process.stdout.write('L' + '\t' + start + '\t' + type + '\t' + rname + '\n');
 
-      process.stdout.write('R' + '\t' + (start + length) + '\t' + type + '\n');
-      process.stdout.write('L' + '\t' + (start + length) + '\t' + type + '\n');
+      process.stdout.write('R' + '\t' + (start + length) + '\t' + type + '\t' + rname + '\n');
+      process.stdout.write('L' + '\t' + (start + length) + '\t' + type + '\t' + rname + '\n');
       return;
     }
-    process.stdout.write('R' + '\t' + bpR + '\t' + type + '\n');
-    process.stdout.write('L' + '\t' + bpL + '\t' + type + '\n');
+    process.stdout.write('R' + '\t' + bpR + '\t' + type + '\t' + rname + '\n');
+    process.stdout.write('L' + '\t' + bpL + '\t' + type + '\t' + rname + '\n');
   });
 
 }
